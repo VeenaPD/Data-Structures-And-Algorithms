a56e1c19aa923a594ab57f87b96e3d9448ec6118
VeenaPD <veenapd4@gmail.com>
2021-05-14 23:45:33 +0530

    recursion problems

---
+++ basics/factorialUsingRecursion.js
@@ -0,0 +1,17 @@
+// Find factorial of given n
+// input: n = 7
+// output: 5040
+// Constraints: n cannot be negative
+
+let n = 7;
+
+function factorialUsingRecursion(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorialUsingRecursion(n - 1);
+  }
+}
+
+let result = factorialUsingRecursion(n);
+console.log(result);
---
+++ basics/fibonacciSeriesUsingRecursion.js
@@ -0,0 +1,22 @@
+// Fibonacci series: A series of numbers whose values are added result of previous two numbers. first and second element are always 0 , 1
+// input: 6
+// output: 0, 1, 1, 2, 3, 5
+
+let n = 6;
+
+function fibonacciSeriesUsingRecursion(n) {
+  if (n < 0) {
+    console.log("n value is invalid");
+  } else if (n < 2) {
+    return n;
+  } else {
+    return (
+      fibonacciSeriesUsingRecursion(n - 1) +
+      fibonacciSeriesUsingRecursion(n - 2)
+    );
+  }
+}
+
+for (let i = 0; i < n; i++) {
+  console.log(fibonacciSeriesUsingRecursion(i));
+}
