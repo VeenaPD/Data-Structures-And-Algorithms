a143d9b9de730bd44c448f66cea09cac137efce4
VeenaPD <veenapd4@gmail.com>
2023-07-13 20:12:32 +0530

    array-string modifications

---
+++ arrays/subarray-of-sum.js
@@ -0,0 +1,32 @@
+// Subarray with given sum
+/**
+ * Given an unsorted array A of size N that contains only non-negative integers,
+ * find a continuous sub-array which adds to a given number S.
+ * Input:
+   N = 5, S = 12
+   A[] = {1,2,3,7,5}
+   Output: 2 4
+   Explanation: The sum of elements 
+   from 2nd position to 4th position 
+   is 12.
+ */
+
+var A = [1, 2, 3, 7, 5];
+var N = 5,
+  S = 12;
+var sortedArray = A.sort();
+var subArray = [];
+var storeItems = [];
+function findSubarray(arr, sum) {
+  for (let i = 0; i < N + 1; i++) {
+    for (let j = i + 1; j < N; j++) {
+      console.log(arr[i], arr[j]);
+      if (arr[i] + arr[j] == sum) {
+        subArray.push(arr[i], arr[j]);
+      }
+    }
+  }
+  return subArray;
+}
+storeItems = findSubarray(sortedArray, S);
+console.log(storeItems);
